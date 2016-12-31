69f4bdc2a97e0c38ee482216e5616bddee567ff2
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-12-31 18:44:06 -0500

    wip; migration not erroring out but not outputting username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.for(:account_update)
+    devise_parameter_sanitizer.permit(:sign_up) << :name
+    devise_parameter_sanitizer.permit(:account_update) << :name
   end
 end
